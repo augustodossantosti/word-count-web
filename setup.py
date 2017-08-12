609026ba811c30156e43e8b6b316e435ac876a78
Augusto Santos <augustodossantos.ti@gmail.com>
2017-08-12 20:17:18 -0300

    Project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='word-count-web',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/augustodossantosti/word-count-web',
+    license='MIT License',
+    author='Augusto Santos',
+    author_email='augustodossantos.ti@gmail.com',
+    description='A simple word count application for web pages with Python and TkInter.'
+)
