c74ca915239aeb392e1cf5172bb58f8965887e47
Bussiness for Zombies <bussiness.for.zombies@lexos.com.br>
2017-08-18 11:57:51 -0300

    Adicionada configuração para o build do projeto + resources

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from distutils.core import setup
+import os
+import sys
+from cx_Freeze import setup, Executable
+
+os.environ['TCL_LIBRARY'] = "C:/Python36x64/tcl/tcl8.6"
+os.environ['TK_LIBRARY'] = "C:/Python36x64/tcl/tk8.6"
+
+include_files = ['resources/autorun.inf', 'C:/Python36x64/DLLs/tcl86t.dll', 'C:/Python36x64/DLLs/tk86t.dll']
+
+build_exe_options = {'packages': [],
+                     'includes': [],
+                     'excludes': [],
+                     'include_files': include_files}
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+executables = [Executable('wordcountweb/word-count-app.py', base=base, icon='resources/favicon.ico')]
 
 setup(
     name='word-count-web',
     version='1.0',
-    packages=[''],
     url='https://github.com/augustodossantosti/word-count-web',
     license='MIT License',
     author='Augusto Santos',
     author_email='augustodossantos.ti@gmail.com',
-    description='A simple word count application for web pages with Python and TkInter.'
+    description='A simple word count application for web pages with Python and TkInter.',
+    options={'build_exe': build_exe_options},
+    executables=executables
 )
